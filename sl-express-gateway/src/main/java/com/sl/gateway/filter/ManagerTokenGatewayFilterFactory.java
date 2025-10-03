41d3da8f4c5c85990463c855060348761f7d3436
LSRrun <1365280451@qq.com>
2025-10-03 17:02:33 +0800

    修复权限问题

--- sl-express-gateway/src/main/java/com/sl/gateway/filter/ManagerTokenGatewayFilterFactory.java
+++ sl-express-gateway/src/main/java/com/sl/gateway/filter/ManagerTokenGatewayFilterFactory.java
@@ -1,13 +1,18 @@
 package com.sl.gateway.filter;
 
+import cn.hutool.core.collection.CollUtil;
+import com.itheima.auth.factory.AuthTemplateFactory;
+import com.itheima.auth.sdk.common.Result;
 import com.itheima.auth.sdk.dto.AuthUserInfoDTO;
 import com.itheima.auth.sdk.service.TokenCheckService;
 import com.sl.gateway.config.MyConfig;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * 后台管理员token拦截处理
@@ -19,6 +24,8 @@ public class ManagerTokenGatewayFilterFactory extends AbstractGatewayFilterFacto
     private MyConfig myConfig;
     @Resource
     private TokenCheckService tokenCheckService;
+    @Value("${role.manager}")
+    private List<Long> managerRoleIds;
 
     @Override
     public GatewayFilter apply(Object config) {
@@ -34,6 +41,11 @@ public class ManagerTokenGatewayFilterFactory extends AbstractGatewayFilterFacto
 
     @Override
     public Boolean auth(String token, AuthUserInfoDTO authUserInfoDTO, String path) {
-        return true;
+        // 根据token和userID查询权限管家 得到角色集合列表
+        Result<List<Long>> result = AuthTemplateFactory.get(token).opsForRole().findRoleByUserId(authUserInfoDTO.getUserId());
+        //读取nacos中manager平台可以访问的角色列表
+        List<Long> userRoleIds = result.getData();
+        //两个角色列表取交集 有交集就可以登录
+        return CollUtil.isNotEmpty(CollUtil.intersection(managerRoleIds,userRoleIds));
     }
 }
